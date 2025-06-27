7434bf05a678af8e73090e905e99e4416a75cfaa
franck <carnet.franck.paul@gmail.com>
2025-06-27 13:16:53 +0200

    Use url(index.php?tf=...) and add overload in define, DC 2.35+

--- _define.php
+++ _define.php
@@ -16,8 +16,9 @@ $this->registerModule(
     '5.3',
     [
         'date'     => '2025-05-15T18:46:32+0200',
-        'requires' => [['core', '2.28']],
+        'requires' => [['core', '2.35']],
         'type'     => 'theme',
+        'overload' => true,
 
         'details'    => 'https://open-time.net/?q=gris_gris',
         'support'    => 'https://github.com/franck-paul/gris_gris',
