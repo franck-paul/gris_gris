5c70a8f419aac7118018d48f5313ac6d4b1a80a8
franck <carnet.franck.paul@gmail.com>
2026-02-24 11:27:58 +0100

    New release 7.1

--- _define.php
+++ _define.php
@@ -13,9 +13,9 @@ $this->registerModule(
     'Gris-Gris',
     'Deviation Cafe-Creme, Structure Aorakit-1B de Kozlika',
     'Franck Paul',
-    '7.0',
+    '7.1',
     [
-        'date'     => '2025-09-07T14:19:42+0200',
+        'date'     => '2026-02-24T11:27:58+0100',
         'requires' => [['core', '2.36']],
         'type'     => 'theme',
         'overload' => true,
