f5341164fcc3ef297be02c6ad24a27e8c72158bf
franck <contact@open-time.net>
2026-08-13 16:48:56 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -9,20 +9,24 @@
  * @copyright Franck Paul (contact@open-time.net)
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'Gris-Gris',
-    'Deviation Cafe-Creme, Structure Aorakit-1B de Kozlika',
-    'Franck Paul',
-    '7.2',
-    [
-        'date'     => '2026-04-05T11:57:36+0200',
-        'requires' => [['core', '2.36']],
-        'type'     => 'theme',
-        'overload' => true,
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=gris_gris',
-        'support'    => 'https://github.com/franck-paul/gris_gris',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/gris_gris/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Gris-Gris',
+        'Deviation Cafe-Creme, Structure Aorakit-1B de Kozlika',
+        'Franck Paul',
+        '7.2',
+        [
+            'date'     => '2026-04-05T11:57:36+0200',
+            'requires' => [['core', '2.36']],
+            'type'     => 'theme',
+            'overload' => true,
+
+            'details'    => 'https://open-time.net/?q=gris_gris',
+            'support'    => 'https://github.com/franck-paul/gris_gris',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/gris_gris/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
